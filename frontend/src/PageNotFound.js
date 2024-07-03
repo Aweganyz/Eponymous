4d94baef0d3301547a9131674ccb4fb98c1b1944
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2024-07-03 16:26:23 +0530

    Add files via upload

---
+++ frontend/src/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/layouts/Header'
+
+function PageNotFound() {
+  return (
+  <>
+  <Header />
+    <div className='pagenotfound'>
+       PageNotFound
+    </div>
+  </>
+  )
+}
+
+export default PageNotFound
